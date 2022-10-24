13bf712587742b229f3cccff9fa1d28cea6aaad2
Nikita Zhuro <nikitazhuro@yandex.by>
2022-10-24 20:55:44 +0300

    remove prod path

--- src/API/index.js
+++ src/API/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const $host = axios.create({
-  baseURL: 'https://zhuro-converter-server.herokuapp.com/',
-});
-
-// const $host = axios.create({
-//   baseURL: 'http://localhost:5000',
+// const $prodHost = axios.create({
+//   baseURL: 'https://zhuro-converter-server.herokuapp.com/',
 // });
+
+const $host = axios.create({
+  baseURL: 'http://localhost:5000',
+});
 
 export default $host;
